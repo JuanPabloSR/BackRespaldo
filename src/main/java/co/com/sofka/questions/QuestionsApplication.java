695def96d4d245c79f61b8932ec78d0910abdd7c
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-01-10 11:12:31 -0500

    esta sera la vencida

--- src/main/java/co/com/sofka/questions/QuestionsApplication.java
+++ src/main/java/co/com/sofka/questions/QuestionsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuestionsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(QuestionsApplication.class, args);
     }
